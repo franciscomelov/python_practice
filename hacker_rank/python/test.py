142535bfdcfb98c45d7fb0d0918c744963274783
franciscomelov <femva3012@hotmail.com>
2020-10-27 21:28:40 -0600

    challenges

--- hacker_rank/python/test.py
+++ hacker_rank/python/test.py
@@ -1,4 +1,41 @@
-a = 1
+a = 3
 b = 2
+nombre = "juan"
 
-print(f"{a} {b}")
+# print(a +b )
+# print(a - b)
+# print(a * b)
+
+print(""" 
+{}
+{}
+{}
+{}
+
+""".format(a+b, a-b, a*b, nombre))
+
+
+
+'''
+n Es impar  print("raro")
+n enter 2 - 5  print("normal")
+n entre 6 - 20  print("raro")
+n es mayor 20 print("normal")
+
+
+if condicion:
+    si secumple
+elif condicion:
+    si se cumple
+elif condicion:
+    si se cumple
+else:
+    si nada se cumple
+
+if 3 == 3:
+    print("numeros IGUALES")
+else:
+    print("Numeros DIFERENTES")
+
+print("Fin")
+'''
