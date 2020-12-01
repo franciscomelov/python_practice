13c8e768db4167d762cdc51cd74418a1b6feff1b
franciscomelov <femva3012@hotmail.com>
2020-11-30 22:13:05 -0600

    Challeges

--- platzi_challenge/42.code_a_character.py
+++ platzi_challenge/42.code_a_character.py
@@ -1,31 +1,43 @@
 """ 
 EL mago: magia
     ataque: echizo
-    fueza: 50
-    resistencia:25
+    fueza: 5
+    resistencia:2
 
 Sacerdotiza: proteccion
     efectp:proteccion-aumenta*.5 resistencia de una carta atacada
     fuerza:1
-    resistencia: 100
+    resistencia: 5
 
-Emperatriz: aumenta fuerza de otra carta
+: aumenta fuerza de otra carta
     efecto: aumento de fuerza *.3 a otra carta
-    fuerza:
+    fuerza:Emperatriz
     resistencia:
 
 Emperador: quita fuerza a otra carta
     ataque:  golpe
-    fuerza:25
-    resistencia:30
+    fuerza:2
+    resistencia:3
 
 El enamorado: roba una carta
     ataque: golpe limpio
-    fuerza: 40
-    resistencia:20
-
+    fuerza: 4
+    resistencia:2
 
 Ataque:
 resistencia 10-100
 Fuerza, 1-50
  """
+
+class Card:
+    def __init__(self, nombre, fuerza, defensa):
+        self.nombre = nombre
+        self.fuerza =fuerza
+        self.defensa=defensa
+
+mago= Card("Mago", 5, 2 )
+sacerdotiza=Card("Sacerdotiza", 1, 5)
+emperatriz=Card("Emperatriz",3, 2 )
+emperador=Card("Emperador", 2, 3)
+enamorado=Card("Enamorado", 4, 2)
+print(mago)
