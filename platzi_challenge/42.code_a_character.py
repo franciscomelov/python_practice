8a14b719dd4e382b94053f5700042ff2dfca061c
franciscomelov <femva3012@hotmail.com>
2020-11-30 21:51:26 -0600

    CHallenges

---
+++ platzi_challenge/42.code_a_character.py
@@ -0,0 +1,31 @@
+""" 
+EL mago: magia
+    ataque: echizo
+    fueza: 50
+    resistencia:25
+
+Sacerdotiza: proteccion
+    efectp:proteccion-aumenta*.5 resistencia de una carta atacada
+    fuerza:1
+    resistencia: 100
+
+Emperatriz: aumenta fuerza de otra carta
+    efecto: aumento de fuerza *.3 a otra carta
+    fuerza:
+    resistencia:
+
+Emperador: quita fuerza a otra carta
+    ataque:  golpe
+    fuerza:25
+    resistencia:30
+
+El enamorado: roba una carta
+    ataque: golpe limpio
+    fuerza: 40
+    resistencia:20
+
+
+Ataque:
+resistencia 10-100
+Fuerza, 1-50
+ """
