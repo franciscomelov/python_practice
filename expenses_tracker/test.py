0cdd6610850dbd052acb61c7c713f60d1969f20c
franciscomelov <femva3012@hotmail.com>
2020-12-03 22:59:30 -0600

    expenses tracker

--- expenses_tracker/test.py
+++ expenses_tracker/test.py
@@ -1,16 +1,3 @@
-import ast
+test = "desayuno, lacteo"
 
-# Guardar diccionario a txt  -- se convierte a string
-""" dict = [{"hola":"adios"}]
-f = open("db.txt","w")
-f.write( str(dict))
-f.close() """
-
-# Leer diccionario como string y lo convierte a diccionaio
-whip =None
-with open('db.txt', 'r') as f:
-    s = f.read()
-    whip = ast.literal_eval(s)
-    print(whip)
-
-print(whip)
+print(test)
