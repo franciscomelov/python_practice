ce75e6f6a0f747b9b95f0c1b4d13b04f25f113ef
franciscomelov <femva3012@hotmail.com>
2020-12-30 15:15:42 -0600

    challenges

--- exercises/test.py
+++ exercises/test.py
@@ -1,27 +1,56 @@
-import math
-def run(simbol, lines):
-    if lines%2 == 0:
-        lines +=1
-    half = math.floor(lines/2)
+primes = [2, 3]
 
-    for line in range(1,half+1):
-        print(simbol * line)
+def is_prime(test):
+    for prime in primes:   #checando sin son divisibles entre algun numero de la lista de primos
+        if test%prime==0:
+            break
+        elif prime > (test/2):
+            primes.append(test)
+            break 
 
-    for line in range(half+1,0,-1):
-        print(simbol * line)
+def get_primes(number):
+    n = 1
+    
+    while True:
+        is_prime(6*n-1)
+        if 6*n-1 >= number:
+            break
+        is_prime(6*n+1)
+        if 6*n+1 >= number:
+            break
+        n +=1
+
+def b_prime_factor(t, n):
+    factor =[]
+    big_factor=None
+    for number in n: # iterate array n
+        for _ in range(t): #search prime factors
+            
+            if number > primes[-1]:get_primes(number) # if true get primes below number
+
+            if number in primes:
+                factor.append(number)
+                break
+            
+            else:
+                for prime in primes:
+                    if number%prime==0:
+                        big_factor=prime
+                    if prime > number/3:
+                        break
+                factor.append(big_factor)
+                break
+
+    for num in factor:
+        print(num)
+
+t = 3
+n=[17,50, 10, 10000, 55551, 111111]
+
+b_prime_factor(t, n)
 
 
-if __name__ == "__main__":
-    simbol = input("Que simbolo quieres usar: ")
-    lines = int(input("Cuantos saltos de linea quieres(numero impar): "))
-    run(simbol, lines)
 
-
-"""
-Mostrar en pantalla 
-#####
-####
-###
-##
-#
-"""
+# take a number from n
+# if number is prime return number
+# else get the biggest factor prime number/ biggest prime == 2
--- platzi_challenge/12.proximo_cumple.py
+++ platzi_challenge/12.proximo_cumple.py
@@ -3,21 +3,44 @@ from datetime import date
 
 def birthday(your_bday):
     your_bday = your_bday.split("-") #separa your_bday en array [mes, dia]
-    year = date.today().year #guarda en year el año actual
-    your_bday = date(year, int(your_bday[0]), int(your_bday[1])) # date(year, month, day) convierte num to real date
+    your_bday = date(int(your_bday[0]), int(your_bday[1]), int(your_bday[2])) # date(year, month, day) convierte num to real date
     today = date.today()  # guarda en today la fecha actual
     days = (your_bday - today).days  # resta a la fecha de tu cumpleños la fecha actual,  #days regresa fecha y hora con .days solo regresa dias
 
-    if days < 0: 
-        your_bday = str(your_bday).split("-")
-        your_bday = date(int(year) +1, int(your_bday[1]), int(your_bday[2]))
-        days = (your_bday - today).days
-        return days , " dias para tu cumpleños"
+
 
     return days ," dias para tu cumpleños"
 
 
 if __name__ == "__main__":
-    your_bday = "1-13" #mes - dia
+    your_bday = "2021-12-22" #mes - dia
     days = birthday(your_bday)
     print(days)
+
+
+# from datetime import date
+
+
+# def birthday(your_bday):
+#     your_bday = your_bday.split("-") #separa your_bday en array [mes, dia]
+#     year = date.today().year #guarda en year el año actual
+#     your_bday = date(year, int(your_bday[0]), int(your_bday[1])) # date(year, month, day) convierte num to real date
+#     today = date.today()  # guarda en today la fecha actual
+#     days = (your_bday - today).days  # resta a la fecha de tu cumpleños la fecha actual,  #days regresa fecha y hora con .days solo regresa dias
+
+#     if days < 0: 
+#         your_bday = str(your_bday).split("-")
+#         your_bday = date(int(year) +1, int(your_bday[1]), int(your_bday[2]))
+#         days = (your_bday - today).days
+#         return days , " dias para tu cumpleños"
+
+#     return days ," dias para tu cumpleños"
+
+
+# if __name__ == "__main__":
+#     your_bday = "1-13" #mes - dia
+#     days = birthday(your_bday)
+#     print(days)
+
+
+
